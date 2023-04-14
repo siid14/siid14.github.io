4c4c0d2d80d8e2a06bac6fb609a968035d2d029e
Sidney <sid.14boys@yahoo.fr>
2023-04-14 15:11:11 -0700

    B; Click + Saved Butto State

---
+++ js/index-client-side.js
@@ -0,0 +1,28 @@
+console.log("Script loaded successfully!");
+
+
+
+function countClicks() {
+  console.log("function countClicks!");
+  var clicks = localStorage.getItem('clickCount');
+  clicks = parseInt(clicks) || 0;
+  clicks += 1;
+  localStorage.setItem('clickCount', clicks);
+  document.getElementById("clickCount").innerHTML = clicks;
+}
+
+
+const button = document.querySelector('button');
+
+// add a click event listener to the button
+button.addEventListener('click', function() {
+  console.log('Button clicked!');
+  countClicks();
+});
+
+
+// initialize the click count display
+window.onload = function() {
+  var clicks = localStorage.getItem('clickCount');
+  document.getElementById("clickCount").innerHTML = clicks || 0;
+};
